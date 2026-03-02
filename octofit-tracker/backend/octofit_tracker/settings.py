2498b04072ebf5d906367d96f858ad9bd18d837e
AshleyBT <ashley.beattie@gmail.com>
2026-03-02 11:20:42 +0000

    Update API URLs to use CODESPACE_NAME and allow Codespace/localhost in ALLOWED_HOSTS

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-hea7v!6d%tuufq9zkv!j-^qi+=+2=cf=a0$a#+0a_)29md*17g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+# Allow localhost and Codespace public URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
